b165da8f43f5b8b9bd6151db7d3c93dfb3aa12cf
Jules f4iey <93203507+f4iey@users.noreply.github.com>
2024-08-26 15:28:22 +0200

    edit translation call

--- plugins/LibreTranslate/src/patches/ActionSheet.tsx
+++ plugins/LibreTranslate/src/patches/ActionSheet.tsx
@@ -55,7 +55,7 @@ export default () => before("openLazy", LazyActionSheet, ([component, key, msg])
                     const target_lang = settings.target_lang
                     const isTranslated = translateType === "Translate"
 
-                    const translate = await LibreTranslate.translate(originalMessage.content, undefined, target_lang, !isTranslated)
+                    const translate = await LibreTranslate.translate(originalMessage.content, undefined, target_lang, "text", 3, "")
 
                     FluxDispatcher.dispatch({
                         type: "MESSAGE_UPDATE",
